597cafdf51583f0beb6f0c4fd3796b86a4947d3e
dlguswo333 <dlguswo002@gmail.com>
2025-01-26 19:48:04 +0900

    Improve FileItem component

--- src/app/FileItem.tsx
+++ src/app/FileItem.tsx
@@ -1,5 +1,11 @@
 'use client';
 
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemText from "@mui/material/ListItemText";
+import DescriptionIcon from '@mui/icons-material/Description';
+import { useMemo } from "react";
+
 type Props = {
   file: File;
 }
@@ -7,10 +13,24 @@ type Props = {
 const FileItem = ({file}: Props) => {
   const {name, size} = file;
 
-  return <div>
-    <div>{name}</div>
-    <div>{size}</div>
-  </div>
+  const prettySize = useMemo(() => {
+    const UNITS = ['B', 'KB', 'MB', 'GB'];
+    const SEP = 1024;
+    let tmpSize = size;
+    let index = 0;
+    while (tmpSize >= SEP && index < UNITS.length) {
+      tmpSize /= SEP;
+      index++;
+    }
+    return `${tmpSize.toFixed(2).replace(/\.00$/, '')}${UNITS[index]}`;
+  }, [size])
+
+  return <ListItem>
+    <ListItemAvatar>
+      <DescriptionIcon />
+    </ListItemAvatar>
+    <ListItemText primary={name} secondary={prettySize} />
+  </ListItem>
 };
 
 export default FileItem;
--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -2,8 +2,10 @@
 import styles from '@/app/upload/page.module.css';
 import useUpload from './useUpload';
 import Button from '@mui/material/Button';
-import FileItem from '../FileItem';
-import { useRef } from 'react';
+import List from '@mui/material/List';
+import FileItem from '@/app/FileItem';
+import { Fragment, useRef } from 'react';
+import { Divider } from '@mui/material';
 
 export default function Upload() {
   const inputRef = useRef<HTMLInputElement | null>(null);
@@ -26,9 +28,15 @@ export default function Upload() {
         <div>Drag and drop files here.</div>
         <input type='file' multiple={true} onChange={onChangeEvent} ref={inputRef} />
       </div>
-      <div>
-        {files?.map(file => (<FileItem key={file.name} file={file} />))}
-      </div>
+      {!!files && <List
+        sx={{minWidth: '300px', maxWidth: '90vw', border: '1px solid #9999', borderRadius: '8px'}}>
+        {files.map((file, index) => (
+          <Fragment key={index}>
+            <FileItem key={file.name} file={file} />
+            {index !== files.length - 1 && <Divider component='li' />}
+          </Fragment>
+        ))}
+      </List>}
     </main>
   );
 }
