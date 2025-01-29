152a81cd68b0d8d7843b7101d8064eb80566fc1e
dlguswo333 <dlguswo002@gmail.com>
2025-01-29 14:28:23 +0900

    Implement basic upload logics on client

--- src/app/upload/useUpload.ts
+++ src/app/upload/useUpload.ts
@@ -1,4 +1,7 @@
+import {blobToBase64} from '@/util';
 import {ChangeEvent, DragEvent, RefObject, useCallback, useEffect, useState} from 'react';
+import {IdObject, NonEmptyString, STFEntryFromClient, STFFileFromClient} from '@/types';
+import {z} from 'zod';
 
 const excludeFolders = async (files: File[]) => {
   return (
@@ -11,6 +14,19 @@ const excludeFolders = async (files: File[]) => {
       }
     }))
   ).filter(result => !!result);
+};
+
+const getStfFile = async (file: File, fileId: string, entryId: string, blob: Blob, chunkInd: number) => {
+  const stfFile = {
+    // Do not pass empty id; pass undefined instead.
+    id: fileId || undefined,
+    entryId,
+    name: file.name,
+    size: file.size,
+    chunk: await blobToBase64(blob),
+    chunkInd,
+  } satisfies z.infer<typeof STFFileFromClient>;
+  return stfFile;
 };
 
 type Props = {
@@ -58,10 +74,67 @@ const useUpload = ({inputRef}: Props) => {
   }, [inputRef]);
 
   const upload = useCallback(async () => {
-    // [TODO]
-    console.log('upload');
-    setStatus('uploading');
-  }, []);
+    try {
+      setStatus('uploading');
+      if (files === null) {
+        return;
+      }
+      const deleteDate = new Date();
+      // [TODO] Allow users to customize it.
+      deleteDate.setDate(deleteDate.getDate() + 3);
+      const entry = {
+        length: files.length,
+        deleteDate: deleteDate.toISOString(),
+      } satisfies z.infer<typeof STFEntryFromClient>;
+      const res = await fetch('/api/upload', {
+        method: 'POST',
+        body: JSON.stringify(entry)
+      });
+      if (!res.ok) {
+        throw new Error();
+      }
+      const {id: entryId} = IdObject.parse(await res.json());
+
+      for (const file of files) {
+        let readBytes = 0;
+        let chunkInd = 0;
+        let fileId = '';
+        if (file.size === 0) {
+          const emptyBlob = new Blob();
+          const stfFile = await getStfFile(file, fileId, entryId, emptyBlob, chunkInd);
+          const res = await fetch('/api/upload', {
+            method: 'POST',
+            body: JSON.stringify(stfFile),
+          });
+          if (!res.ok) {
+            throw new Error();
+          }
+          continue;
+        }
+        while (readBytes < file.size) {
+          const CHUNK_SIZE = 1024 * 1024;
+          const chunk = file.slice(readBytes, readBytes + CHUNK_SIZE);
+          readBytes += chunk.size;
+          const stfFile = await getStfFile(file, fileId, entryId, chunk, chunkInd);
+          const res = await fetch('/api/upload', {
+            method: 'POST',
+            body: JSON.stringify(stfFile),
+          });
+          if (!res.ok) {
+            throw new Error();
+          }
+          fileId = fileId || IdObject.parse(await res.json()).id;
+          // fileId must be non empty string here.
+          NonEmptyString.parse(fileId);
+          chunkInd++;
+        }
+      }
+      setStatus('idle');
+    } catch (e) {
+      console.error(e);
+      setStatus('uploadError');
+    }
+  }, [files]);
 
   useEffect(() => {
     const isOkay = !!files && files?.length > 0;
