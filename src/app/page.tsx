57296330dedc21790df451878f51028158d9c2b1
dlguswo333 <dlguswo002@gmail.com>
2025-02-03 23:15:32 +0900

    Add a little fancy title in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,13 +11,41 @@ export default function Home () {
     <AppBar position='sticky'>
       <Toolbar sx={{justifyContent: 'space-between'}}>
         <span />
-        <Typography component='h1'>
-          Share Those Files!
-        </Typography>
+        <span>
+          <Typography component='h1' className={styles.headerTitle}>
+            share-those-files!
+          </Typography>
+        </span>
         <span />
       </Toolbar>
     </AppBar>
     <main className={styles.page}>
+      <Typography component='h1'
+        className={styles.pageTitle}
+        sx={{
+          marginBottom: '20px',
+          transform: 'skew(5deg)',
+          animation: `ease ${styles.hover} 5s infinite`,
+          userSelect: 'none',
+          fontWeight: 'bold',
+          fontSize: '4rem',
+          lineHeight: 1,
+          color: 'white',
+          textShadow: `
+            1px 1px #000,
+            1px -1px #000,
+            -1px 1px #000,
+            -1px -1px #000,
+            2px 2px #25f,
+            3px 3px #25f,
+            4px 4px #25f,
+            5px 5px #25f`,
+        }}
+      >
+          share-<br />
+          those-<br />
+          files!<br />
+      </Typography>
       <div className={styles.buttonContainer}>
         <DownloadPageButton />
         <Link legacyBehavior={true} passHref={true} href='/upload'>
